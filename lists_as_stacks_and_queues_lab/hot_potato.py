0338f8c291da5f8d619fde036d979be5abe1d239
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2022-06-06 18:09:08 +0200

    Add files via upload

---
+++ lists_as_stacks_and_queues_lab/hot_potato.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+kids_str = input()
+tosses = int(input())
+
+kids = deque(kids_str.split(" "))
+tosses_count = int(tosses)
+
+current_count = 0
+while len(kids) > 1:
+    current_count += 1
+    kid = kids.popleft()
+    if current_count < tosses_count:
+        kids.append(kid)
+    else:
+        print(f"Removed {kid}")
+        current_count = 0
+print(f"Last is {kids.popleft()}")
