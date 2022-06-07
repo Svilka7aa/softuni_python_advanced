2e947dda0818ea69684feb626c90279715bf4c39
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2022-06-07 21:46:34 +0200

    Add files via upload

---
+++ lists_as_stacks_and_queues_exercise/cups_and_bottles.py
@@ -0,0 +1,8 @@
+from collections import deque
+
+cups = deque([int(x) for x in input().split()])
+bottles = [int(x) for x in input().split()]
+wasted_water = 0
+
+for bottle in reversed(bottles):
+    pass
