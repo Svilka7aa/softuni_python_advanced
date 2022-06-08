3ec3e01e15d2dca5e743fce6a1db5cd3a45e0df9
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2022-06-08 17:33:01 +0200

    Add files via upload

---
+++ tuples_and_sets_lab/students_grades.py
@@ -0,0 +1,21 @@
+def avg(values):
+    return sum(values) / len(values)
+
+
+n = int(input())
+student_strings = [input() for _ in range(n)]
+
+students_grades = {}
+
+for student_string in student_strings:
+    student, grade_string = student_string.split(" ")
+    grade = float(grade_string)
+    if student not in students_grades:
+        students_grades[student] = []
+
+    students_grades[student].append(grade)
+
+for student, grades in students_grades.items():
+    grades_formatted = " ".join(f'{grade:.2f}' for grade in grades)
+    grades_avg = avg(grades)
+    print(f"{student} -> {grades_formatted} (avg: {grades_avg:.2f})")
