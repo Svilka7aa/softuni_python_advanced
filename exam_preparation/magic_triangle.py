1f98a608326d6047a0e43bb51ceac8c7f544a1c8
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-04-11 22:29:04 +0200

    Add files via upload

--- exam_preparation/magic_triangle.py
+++ exam_preparation/magic_triangle.py
@@ -1,15 +1,14 @@
-def get_magic_triangle(num):
-    triangle = []
-
-    for n in range(num):
-        triangle.append([])
-        triangle[n].append(1)
+def get_magic_triangle(num_rows):
+    result = []
+    for n in range(num_rows):
+        result.append([])
+        result[n].append(1)
         for x in range(1, n):
-            triangle[n].append(triangle[n - 1][x - 1] + triangle[n - 1][x])
+            result[n].append(result[n - 1][x -1] + result[n - 1][x])
         if n != 0:
-            triangle[n].append(1)
+            result[n].append(1)
 
-    return triangle
+    return result
 
 
 get_magic_triangle(5)
