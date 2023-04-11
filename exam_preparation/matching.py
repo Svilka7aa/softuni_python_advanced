3e274b27a4f38d839fa18a33f424043beaf3a5be
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-04-11 22:29:25 +0200

    Add files via upload

--- exam_preparation/matching.py
+++ exam_preparation/matching.py
@@ -2,33 +2,45 @@ from collections import deque
 
 males = [int(x) for x in input().split()]
 females = deque([int(x) for x in input().split()])
-
 matches = 0
 
-while males and females:
-    male = males.pop()
-    if male <= 0:
-        continue
-    elif male % 25 == 0:
-        males.pop()
-        continue
-    female = females.popleft()
-    if female <= 0:
-        males.append(male)
-        continue
-    elif female % 25 == 0:
-        females.popleft()
+
+while females and males:
+    current_female = females.popleft()
+    current_male = males.pop()
+
+    if current_female <= 0:
+        males.append(current_male)
         continue
 
-    if male == female:
+    if current_male <= 0:
+        females.appendleft(current_female)
+        continue
+
+    if current_female % 25 == 0:
+        females.popleft()
+        males.append(current_male)
+        continue
+
+    if current_male % 25 == 0:
+        males.pop()
+        females.appendleft(current_female)
+        continue
+
+    if current_female != current_male:
+        males.append(current_male - 2)
+
+    elif current_female == current_male:
         matches += 1
-    else:
-        males.append(male - 2)
 
 print(f"Matches: {matches}")
-if len(males) == 0:
-    males.append("none")
-print(f"Males left: {', '.join(str(x) for x in reversed(males))}")
-if len(females) == 0:
-    females.append("none")
-print(f"Females left: {', '.join(str(x) for x in females)}")
+if males:
+    males_left = reversed(males)
+    print(f"Males left: {', '.join(str(x) for x in males_left)}")
+else:
+    print(f"Males left: none")
+if females:
+    print(f'Females left: {", ".join(str(x) for x in females)}')
+else:
+    print("Females left: none")
+
