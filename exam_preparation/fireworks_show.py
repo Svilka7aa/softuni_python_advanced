df86d3c8f988c5053709fdc9e8de4fcd82c5f6ce
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-04-26 17:30:04 +0200

    Add files via upload

--- exam_preparation/fireworks_show.py
+++ exam_preparation/fireworks_show.py
@@ -1,7 +1,7 @@
 from collections import deque
 
-firework_effects = deque([int(x) for x in input().split(", ")])
-explosive_power = [int(x) for x in input().split(", ")]
+fireworks_effects = deque(int(x) for x in input().split(", "))
+explosive_power = deque(int(x) for x in input().split(", "))
 
 fireworks_dict = {
     "Palm Fireworks": 0,
@@ -9,14 +9,14 @@ fireworks_dict = {
     "Crossette Fireworks": 0
 }
 
-while firework_effects and explosive_power:
-    current_firework = firework_effects.popleft()
+while fireworks_effects and explosive_power:
+    current_firework = fireworks_effects.popleft()
     if current_firework <= 0:
         continue
 
     current_explosion = explosive_power.pop()
     if current_explosion <= 0:
-        firework_effects.appendleft(current_firework)
+        fireworks_effects.appendleft(current_firework)
         continue
 
     current_sum = current_firework + current_explosion
@@ -32,18 +32,19 @@ while firework_effects and explosive_power:
 
     else:
         current_firework -= 1
-        firework_effects.append(current_firework)
+        fireworks_effects.append(current_firework)
         explosive_power.append(current_explosion)
+
 
 if all(value >= 3 for value in fireworks_dict.values()):
     print("Congrats! You made the perfect firework show!")
 else:
     print("Sorry. You can't make the perfect firework show.")
-
-if firework_effects:
-    print(f"Firework Effects left: {', '.join(str(x) for x in firework_effects)}")
+if fireworks_effects:
+    print(f"Firework Effects left: {', '.join(str(x) for x in fireworks_effects)}")
 if explosive_power:
     print(f"Explosive Power left: {', '.join(str(x) for x in explosive_power)}")
 
 for key, value in fireworks_dict.items():
     print(f"{key}: {value}")
+
