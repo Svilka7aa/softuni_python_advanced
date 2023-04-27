a3f593ee122f59866f8ae596fe66d9f0cbe686f3
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-04-27 20:49:41 +0200

    Add files via upload

--- exam_preparation/north_pole_challenge.py
+++ exam_preparation/north_pole_challenge.py
@@ -1,4 +1,4 @@
-def move_santa(direction, row, col):
+def move(direction, row, col):
     if direction == "left":
         return row, col - 1
     if direction == "right":
@@ -13,7 +13,7 @@ def is_outside(row, col, rows, cols):
     return row < 0 or col < 0 or row >= rows or col >= cols
 
 
-def reposition_santa(row, col, rows, cols):
+def reposition(row, col, rows, cols):
     if row < 0:
         return rows - 1, col
     if row >= rows:
@@ -25,51 +25,53 @@ def reposition_santa(row, col, rows, cols):
 
 
 rows, cols = [int(x) for x in input().split(", ")]
-area = []
-christmas = False
+workshop = []
 santa_row, santa_col = 0, 0
-christmas_decorations = 0
+decorations = 0
 gifts = 0
 cookies = 0
 items = 0
+christmas = False
 
 for row in range(rows):
     row_elements = input().split()
     for col in range(cols):
         if row_elements[col] == "Y":
             santa_row, santa_col = row, col
-        elif row_elements[col] == "D" or row_elements[col] == "C" or row_elements[col] == "G":
+        elif row_elements[col] == "D" or row_elements[col] == "G" or row_elements[col] == "C":
             items += 1
-    area.append(row_elements)
+    workshop.append(row_elements)
 
 command = input()
 
 while True:
-    if command == "End" or command == "":
+    if command == "End" or command == " ":
         break
 
     command_parts = command.split("-")
     direction = command_parts[0]
     steps = int(command_parts[1])
 
-    for move in range(steps):
-        area[santa_row][santa_col] = "x"
-        santa_row, santa_col = move_santa(direction, santa_row, santa_col)
+    for step in range(steps):
+        workshop[santa_row][santa_col] = "x"
+        santa_row, santa_col = move(direction, santa_row, santa_col)
 
         if is_outside(santa_row, santa_col, rows, cols):
-            santa_row, santa_col = reposition_santa(santa_row, santa_col, rows, cols)
+            santa_row, santa_col = reposition(santa_row, santa_col, rows, cols)
 
-        if area[santa_row][santa_col] == "D":
-            christmas_decorations += 1
+        if workshop[santa_row][santa_col] == "D":
+            decorations += 1
             items -= 1
-        elif area[santa_row][santa_col] == "G":
-            gifts += 1
-            items -= 1
-        elif area[santa_row][santa_col] == "C":
+
+        elif workshop[santa_row][santa_col] == "C":
             cookies += 1
             items -= 1
 
-        area[santa_row][santa_col] = "Y"
+        elif workshop[santa_row][santa_col] == "G":
+            gifts += 1
+            items -= 1
+
+        workshop[santa_row][santa_col] = "Y"
 
         if items == 0:
             print("Merry Christmas!")
@@ -81,9 +83,12 @@ while True:
 
     command = input()
 
-print("You've collected:")
-print(f"- {christmas_decorations} Christmas decorations")
+print(f"You've collected:")
+print(f"- {decorations} Christmas decorations")
 print(f"- {gifts} Gifts")
 print(f"- {cookies} Cookies")
-for row in area:
-    print(*row, sep=" ")
+
+for row in workshop:
+    print(" ".join(row), sep=" ")
+
+
