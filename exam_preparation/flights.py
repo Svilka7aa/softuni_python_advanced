dd06118b998f77feb3317320ef10470ce31cfc57
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-04-29 20:37:47 +0200

    Add files via upload

--- exam_preparation/flights.py
+++ exam_preparation/flights.py
@@ -2,7 +2,6 @@ def flights(*args):
     flights_dict = {}
 
     for index, flight in enumerate(args):
-
         if flight == "Finish":
             break
 
@@ -16,3 +15,6 @@ def flights(*args):
 
 
 print(flights('Vienna', 256, 'Vienna', 26, 'Morocco', 98, 'Paris', 115, 'Finish', 'Paris', 15))
+print(flights('London', 0, 'New York', 9, 'Aberdeen', 215, 'Sydney', 2, 'New York', 300, 'Nice', 0, 'Finish'))
+print(flights('Finish', 'New York', 90, 'Aberdeen', 300, 'Sydney', 0))
+
